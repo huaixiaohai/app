c5c1439174938e052abca3d46757292db87c50b2
lzx <zhanxiong.liu@mthreads.com>
2022-04-19 18:55:33 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <template>
-<div >
-  <!-- <ul id="main">
-    <li><router-link  to="/" >商品</router-link></li>
-    <li><router-link  to="/about">评价</router-link></li>
-  </ul> -->
-  <router-view />
-</div>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>  main </el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
 </template>
 
 <script>
@@ -16,25 +14,50 @@ export default {
 </script>
 
 <style>
-/* html{   
+html{   
     width: 100%;   
     height: 100%;   
     overflow: hidden;   
     font-style: sans-serif;   
-}   
+}
 body{   
     width: 100%;   
     height: 100%;   
     font-family: 'Open Sans',sans-serif;   
     margin: 0;   
     background-color: #4A374A;   
-}  */
+} 
+
+.el-container{
+    /*设置内部填充为0，几个布局元素之间没有间距*/
+    padding: 0px;
+      /*外部间距也是如此设置*/
+    margin: 0px;
+    /*统一设置高度为100%*/
+    height: 100%;
+}
+
+.el-header, .el-footer {
+    
+  background-color: #B3C0D1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+.el-main {
+    
+  background-color: #E9EEF3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
 #app {
+  height: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
